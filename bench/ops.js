bf6fca12c84d08e298324c150a02d0ad80337242
Tamik <hello@Tamik.ru>
2020-09-27 19:29:48 +0300

    Updated bench ops script

--- bench/ops.js
+++ bench/ops.js
@@ -5,19 +5,23 @@ const Benchmark = require('benchmark');
 
 const nodeObjectHashParams = {alg: 'md5', coerce: false};
 
-const hashit = require('..').hashit;
 const hashObject = require('hash-object');
 const objectHash = require('object-hash').MD5;
 const nodeObjectHash = require('node-object-hash')(nodeObjectHashParams).hash;
 
-let testCases = {
+const {hashit} = require('../dist/cjs');
+
+const testCases = {
     array: new Array(10).fill('sdfgsdfg'),
     object: {
-        a: 'dfgsdfg'
+        a: 'dfgsdfg',
     },
-    nestedObject: deepThrow({
-        a: 'rwter'
-    }, 100),
+    nestedObject: deepThrow(
+        {
+            a: 'rwter',
+        },
+        100,
+    ),
     complexObject_5items: new Array(5).fill(1).reduce(acc => acc.concat(getFakeRealData()), []),
     complexObject_10items: new Array(10).fill(1).reduce(acc => acc.concat(getFakeRealData()), []),
     complexObject_100items: new Array(100).fill(1).reduce(acc => acc.concat(getFakeRealData()), []),
@@ -25,7 +29,6 @@ let testCases = {
     // hashObject can't hash Map, Set and typed arrays
     // set: new Set(['adfs', 'wer', 'sdf', 'bcvb', 'yutr']),
     // map: new Map([['fgdsfg', 'rtyre'], ['adsasf', 'tyer'], ['dfgddf', 'rtyr']]),
-
 };
 
 let results = {};
@@ -35,10 +38,14 @@ const testCasesNames = Object.keys(testCases);
 const hashitParams = {sortArrays: true, includePrimitiveTypes: true};
 
 console.log(`Warming up...`);
+
 for (const caseName of testCasesNames) {
     warmUp(hashit, hashitParams, 1000, testCases[caseName]);
+
     warmUp(nodeObjectHash, nodeObjectHashParams, 1000, testCases[caseName]);
+
     warmUp(hashObject, null, 1000, testCases[caseName]);
+
     warmUp(objectHash, null, 1000, testCases[caseName]);
 }
 
@@ -49,7 +56,7 @@ function doCase(caseNameIndex) {
 
     const caseName = testCasesNames[caseNameIndex];
     const testData = testCases[caseName];
-    const suite = new Benchmark.Suite;
+    const suite = new Benchmark.Suite();
 
     if (!caseName) {
         for (const key of Object.keys(results).sort((r1, r2) => results[r1].length < results[r2].length)) {
@@ -61,7 +68,7 @@ function doCase(caseNameIndex) {
     console.log(`Benchmarking ${caseName} case...`);
 
     suite
-        .add(`hashit/${caseName}`, function() {
+        .add(`hashit/${caseName}`, function () {
             hashit(testData, hashitParams);
         })
         .add(`nodeObjectHash/${caseName}`, function () {
@@ -73,10 +80,10 @@ function doCase(caseNameIndex) {
         .add(`objectHash/${caseName}`, function () {
             objectHash(testData);
         })
-        .on('cycle', function(event) {
+        .on('cycle', function (event) {
             console.log(String(event.target));
         })
-        .on('complete', function() {
+        .on('complete', function () {
             for (const name of this.filter('fastest').map('name')) {
                 const lib = name.split('/')[0];
                 if (!results[lib]) {
@@ -88,13 +95,13 @@ function doCase(caseNameIndex) {
 
             doCase(caseNameIndex + 1);
         })
-        .run({ async: true });
+        .run({async: true});
 }
 
 function warmUp(func, params, times, testData) {
     let result;
 
-    while (times --> 0) {
+    while (times-- > 0) {
         result = func(testData, params);
     }
 
@@ -102,9 +109,9 @@ function warmUp(func, params, times, testData) {
 }
 
 function deepThrow(object, depth) {
-    while (depth --> 0) {
+    while (depth-- > 0) {
         let tmp = {
-            object: object
+            object: object,
         };
         object = tmp;
     }
@@ -118,36 +125,34 @@ function getFakeRealData() {
         address: {
             city: faker.address.city(),
             streetAddress: faker.address.streetAddress(),
-            country: faker.address.country()
+            country: faker.address.country(),
         },
-        email: [
-            faker.internet.email(),
-            faker.internet.email(),
-            faker.internet.email(),
-            faker.internet.email()
-        ],
+        email: [faker.internet.email(), faker.internet.email(), faker.internet.email(), faker.internet.email()],
         randoms: [
             faker.random.number(),
             faker.random.alphaNumeric(),
             faker.random.number(),
             faker.random.alphaNumeric(),
             faker.random.words(),
-            faker.random.word()
+            faker.random.word(),
         ],
         avatars: [
             {
                 number: faker.random.number(),
-                avatar: faker.internet.avatar()
-            }, {
+                avatar: faker.internet.avatar(),
+            },
+            {
                 number: faker.random.number(),
-                avatar: faker.internet.avatar()
-            }, {
+                avatar: faker.internet.avatar(),
+            },
+            {
                 number: faker.random.number(),
-                avatar: faker.internet.avatar()
-            }, {
+                avatar: faker.internet.avatar(),
+            },
+            {
                 number: faker.random.number(),
-                avatar: faker.internet.avatar()
-            }
-        ]
+                avatar: faker.internet.avatar(),
+            },
+        ],
     };
 }
