d075be1640cdfb4ac96ff84d41db8dbbd7c8aba7
Tamik <hello@Tamik.ru>
2020-09-27 18:20:14 +0300

    Entrypoint

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import Hasher, {HasherOptions} from './Hasher';
+
+/**
+ * Function for simple hash single value.
+ *
+ * @example
+ * import {hashit} from 'node-hashit';
+ *
+ * hashit({key: 'value', value: 'key'}) === hashit({value: 'key', key: 'value'}); // true
+ * hashit(new Set(['value1', 'value2'])) === hashit(new Set(['value2', 'value1'])); // true
+ * hashit(new Map([['key', 'value'], ['value', 'key']])) === hashit(new Map([['value', 'key'], ['key', 'value']])); // true
+ * hashit([1, 2, 3]) === hashit([1, 2, 3]); // true
+ * hashit([1, 2, 3], {sortArrays: true}) === hashit([1, 3, 2], {sortArrays: true}); // true
+ *
+ * hashit([1, 2, 3]) === hashit([1, 3, 2]); // false
+ * hashit(5) === hashit('5'); // false
+ */
+export function hashit(value: unknown, options?: HasherOptions): string {
+    const hasher = new Hasher(options);
+
+    hasher.update(value);
+
+    return hasher.digest() as string;
+}
+
+export default Hasher;
